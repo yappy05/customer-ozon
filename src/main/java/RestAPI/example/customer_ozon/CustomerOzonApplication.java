079aad8398ccf2967bd4c9b73d95b0ecc83f2637
sergey <wendingnn@gmail.com>
2025-04-21 17:21:16 +0300

    init repo and connect postgresSQL

---
+++ src/main/java/RestAPI/example/customer_ozon/CustomerOzonApplication.java
@@ -0,0 +1,13 @@
+package RestAPI.example.customer_ozon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerOzonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerOzonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/RestAPI/example/customer_ozon/CustomerOzonApplicationTests.java
@@ -0,0 +1,13 @@
+package RestAPI.example.customer_ozon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerOzonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
